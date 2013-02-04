2b6c47a0c17c4f2f42d92354c1eb4cbdbb45c6d4
Pierre-Alexis de Solminihac <pa@solminihac.fr>
2013-02-04 01:50:31 +0100

    restoring contenus-1.5 branch

--- view/contenus/addcontenu.php
+++ view/contenus/addcontenu.php
@@ -2,7 +2,6 @@
 $ns = $this->getModel('fonctions');
 if (isset($data['content'])) {
     $contenu = $data['content'];
-    $nb_types_contenus = 0;
 }
 ?>
 <div class="form_contenu_edit">
@@ -10,32 +9,31 @@ if (isset($data['content'])) {
 if (count($contenu) > 1) {
     foreach ($contenu as $type) {
         $option = substr($type, 1 + strlen($this->getModel('cms')->table_cms_contenu));
-        // si le type de contenu n'a pas de nom on ne le propose pas
-        if (!empty(Clementine::$config['module_contenus'][$option . '_name'])) {
-            ++$nb_types_contenus;
 ?>
-        <p>
-            <a class="<?php echo $option; ?>" href="<?php echo __WWW__; ?>/contenus/addcontenu?id_zone=<?php 
-        if ($ns->ifGet('int', 'id')) { 
-            echo $ns->ifGet('int', 'id'); 
-        } else { 
-            echo '0'; 
-        } 
+    <p>
+        <a class="<?php echo $option; ?>" href="<?php echo __WWW__; ?>/contenus/addcontenu?id_zone=<?php 
+    if ($ns->ifGet('int', 'id')) { 
+        echo $ns->ifGet('int', 'id'); 
+    } else { 
+        echo '0'; 
+    } 
 ?>&id_page=<?php
-        if ($ns->ifGet('id', 'page')) { 
-            echo $ns->ifGet('id', 'page'); 
-        } else { 
-            echo '0'; 
-        } 
+    if ($ns->ifGet('id', 'page')) { 
+        echo $ns->ifGet('id', 'page'); 
+    } else { 
+        echo '0'; 
+    } 
 ?>&type_content=<?php echo $type; ?>"><?php
+    if (isset(Clementine::$config['module_contenus'][$option . '_name'])) {
         echo Clementine::$config['module_contenus'][$option . '_name'];
-?></a>
-        </p>
-<?php 
-        }
+    } else {
+        echo $option;
     }
-}
-if (!$nb_types_contenus) {
+?></a>
+    </p>
+<?php 
+    }
+} else {
     $url = __WWW__ . '/contenus/addcontenu?id_zone=';
     if ($ns->ifGet('int', 'id')) { 
         $url .= $ns->ifGet('int', 'id'); 
